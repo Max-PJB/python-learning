827074d29c5c817351579195aab644239780c66f
pengjb <pengjianbiao@hotmail.com>
2018-10-12 12:06:49 +0800

    test github account

--- json/json.py
+++ json/json.py
@@ -15,17 +15,4 @@
 
 __author__ = 'Max_Pengjb'
 
-a = {"count": 3,
- "infos":
-     [
-         {"name": "赵昊", "age": 15, "height": 1.83, "sex": "男性"},
-         {"name": "龙傲天", "age": 16, "height": 2.00, "sex": "男性"},
-         {"name": "玛丽苏", "age": 15, "height": 1.78, "sex": "女性"}
-     ]
- }
-for i in range(len(a["infos"])):
-    a["infos"][i]["age"] += 1
-    print(a["infos"][i]["age"])
 
-a["infos"].append({"name": "赵昊", "age": 15, "height": 1.83, "sex": "男性"})
-print(a)
